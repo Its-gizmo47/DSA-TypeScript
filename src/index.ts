0e9248c0c10cad4e8baddc812221baf0e1a6be4d
Ali Haider <gorayaali80@gmail.com>
2025-05-31 11:07:07 +0500

    constructNote

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello from TypeScript!");
+function test(message: string, letters: string): boolean {
+    if (message.length > letters.length || letters.length === 1) {
+        return false
+    }
+    let messageCounter: Record<string, number> = {}
+    let letterCounter: Record<string, number> = {}
+
+    for (let value of message) {
+        messageCounter[value] = ++messageCounter[value] || 1
+    }
+
+    for (let value of letters) {
+        letterCounter[value] = ++ letterCounter[value] || 1
+    }
+
+    console.log(messageCounter, letterCounter)
+    for (let key in messageCounter) {
+        if (!(key in letterCounter)) {
+            return false;
+        }
+        if (messageCounter[key] > letterCounter[key]) {
+            return false
+        }
+    }
+
+
+    return true
+}
+
+console.log(test('abaaadc', 'abcdaaa'))
