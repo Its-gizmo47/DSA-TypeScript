a638fcafdfd7c09afa56caea54c0d836becb67d1
Ali Haider <gorayaali80@gmail.com>
2025-05-31 12:25:53 +0500

    is subsequence

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-function averagePair(arr: number[], avg: number): boolean {
+function isSubsequence (str1: string, str2: string): boolean {
 
-    let left = 0;
-    let right = arr.length - 1;
+    if (str1.length > str2.length) {
+        return false
+    }
 
-    while (left < right) {
-        console.log(arr[right], arr[left])
-        let average = arr[left] + arr[right] / 2;
-        if (average === avg) {
-            return true;
-        } else if (average < avg) {
-            left++
-        } else {
-            right--
+    let first = 0;
+    let sec = 0;
+
+    while (sec < str2.length) {
+        console.log(str1[first], str2[sec], first, sec)
+        if (str1[first] === str2[sec]) {
+            first++
         }
+        if (first === str1.length) return true
+        sec++
     }
 
     return false
 }
 
-console.log(averagePair([1,3,3,5,6,7,10,12,19],8))
+console.log(isSubsequence("heallo", "hello there"))
