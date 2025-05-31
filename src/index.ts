6085ac2343bce045fee4da18113e1da245da8209
Ali Haider <gorayaali80@gmail.com>
2025-05-31 12:43:55 +0500

    is subsequence with recursion

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
-function isSubsequence (str1: string, str2: string): boolean {
+function isSubsequenceRecursion(str1: string, str2: string): boolean {
+    console.log(str1, str2)
+    if (str1.length === 0) return true
+    if (str2.length === 0) return false
 
-    if (str1.length > str2.length) {
-        return false
+    let str2Splice = str2.slice(1)
+    if (str1[0] === str2[0]) {
+        return isSubsequenceRecursion(str1.slice(1), str2Splice)
     }
 
-    let first = 0;
-    let sec = 0;
-
-    while (sec < str2.length) {
-        console.log(str1[first], str2[sec], first, sec)
-        if (str1[first] === str2[sec]) {
-            first++
-        }
-        if (first === str1.length) return true
-        sec++
-    }
-
-    return false
+    return isSubsequenceRecursion(str1, str2Splice)
 }
 
-console.log(isSubsequence("heallo", "hello there"))
+console.log(isSubsequenceRecursion("heallo", "hello there"))
