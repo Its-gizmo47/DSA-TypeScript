1949b6856e6f2b783740e3d93b498ed6edf7ea59
Ali Haider <gorayaali80@gmail.com>
2025-06-03 19:13:41 +0500

    divide and conquer sorted frequency

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,63 @@
-function findLongestSubstring(str: string): number {
-    let total = 0
-    let seenChar: Record<string, number> = {}
-    let start = 0
+function sortedFrequency(arr: number[], find: number): number
+{
+    let firstIdx = findFirstIdx(arr, find);
+    if (firstIdx === -1) return firstIdx;
+    let lastIdx = findLastIdx(arr, find, firstIdx);
 
-    for (let i = 0; i < str.length; i ++) {
-        let value = str[i];
-        if (seenChar[value]) {
-            start = Math.max(start, seenChar[value])
-        }
-
-        total = Math.max(total, i - start + 1)
-
-        seenChar[value] = i + 1;
-    }
-    
-    return total;
+    return lastIdx - firstIdx + 1;
 }
 
-console.log(
-    findLongestSubstring(""), // 0
-    findLongestSubstring("rithmschool"), // 7
-    findLongestSubstring("thisisawesome"), // 6
-    findLongestSubstring("thecatinthehat"), // 7
-    findLongestSubstring("bbbbbb"), // 1 
-    findLongestSubstring("longestsubstring"), // 8
-    findLongestSubstring("thisishowwedoit") // 6
-)
 
+function findFirstIdx(arr: number[], find: number): number {
+    const idx = -1;
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === find) {
+            if (arr[mid -1] !== find) {
+                return mid;
+            } else if (arr[mid -1] === find) {
+                right = mid - 1
+            }
+        } else if (arr[mid] < find)  {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return idx;
+}
+
+function findLastIdx(arr: number[], find: number, firstIdx: number): number {
+    const idx = firstIdx;
+    let left = firstIdx;
+    let right = arr.length - 1;
+    let loop = 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === find) {
+            if (arr[mid + 1] !== find) {
+                return mid;
+            } else if (arr[mid + 1] === find) {
+                left = mid + 1
+            }
+        } else if (arr[mid] < find)  {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return idx;
+}
+
+
+
+console.log(
+    sortedFrequency([1,1,2,2,2,2,3],2), // 4 
+    sortedFrequency([1,1,2,2,2,2,3],3), // 1 
+    sortedFrequency([1,1,2,2,2,2,3],1), // 2 
+    sortedFrequency([1,1,2,2,2,2,3],4), // -1
+)
